c207ac685c9c0dadc698e9b4a8f8a1c91cba8d41
Denna Mandela <denna.mandela13@gmail.com>
2022-11-13 16:37:49 +0700

    berhasil di update

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,33 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< HEAD
-const {getAllTodo, getIdTodo, postTodo, updateTodo, deleteIdTodo, deleteTodo} = require ('../controllers/todo.controller');
-
-//fungsi route 
-router.get('/', getAllTodo);
-router.get('/:id', getIdTodo);
-router.post('/', postTodo);
-router.put('/:id', updateTodo);
-router.delete('/:id', deleteIdTodo);
-router.delete('/', deleteTodo);
-=======
 const {
     getAllTodo, 
     getTodoByID, 
     addTodo, 
     updateTodoByID, 
     deleteTodoByID, 
-    deleteAllTodo
+    deleteAll,
 } = require ('../controllers/todo.controller');
 
 //fungsi route 
-router.get("/", getAllTodo);
-router.get("/:id", getTodoByID);
-router.post("/", addTodo);
-router.put("/:id", updateTodoByID);
-router.delete("/:id", deleteTodoByID);
-router.delete("/", deleteAllTodo);
-
-module.exports = router;
->>>>>>> 94a4b76 (menambahkan seeder dan crud)
+router.get('/', getAllTodo);
+router.get('/:id', getTodoByID);
+router.post('/', addTodo);
+router.put('/:id', updateTodoByID);
+router.delete('/:id', deleteTodoByID);
+router.delete('/', deleteAll);
