1856a15a21c34e18df2b94eb49313e1869fdc4b7
Denna Mandela <denna.mandela13@gmail.com>
2022-11-13 16:32:10 +0700

    commit update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const todo = require('./todo.route');
-
-//localhost:3000/todo
-router.use('/todo', todo);
-=======
 
 const todo = require('./todo.route');
 const users = require('./user.route');
@@ -13,7 +7,6 @@ const users = require('./user.route');
 //localhost:3000/todos
 router.use('/todos', todo);
 router.use('/users', users);
->>>>>>> 94a4b76 (menambahkan seeder dan crud)
 
 module.exports = router;
 
